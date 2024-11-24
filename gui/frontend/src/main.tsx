14df01b24363fd514d40d9511e2d11a98c7f4647
Vinicius Egidio <me@vinicius.io>
2024-11-24 07:01:54 +0100

    feat: Add auto detection of dark/light mode

--- gui/frontend/src/main.tsx
+++ gui/frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+import { createTheme, CssBaseline, ThemeProvider, useMediaQuery } from '@mui/material'
 import { LicenseInfo } from '@mui/x-license'
 import App from './App'
 import Events from './Events'
@@ -23,12 +23,20 @@ const darkTheme = createTheme({
 const licenseKey = import.meta.env.VITE_MUI_LICENSE
 LicenseInfo.setLicenseKey(licenseKey)
 
-root.render(
-    <React.StrictMode>
-        <ThemeProvider theme={lightTheme}>
+const Main = () => {
+    const isDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+
+    return (
+        <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
             <Events />
             <CssBaseline />
             <App />
         </ThemeProvider>
+    )
+}
+
+root.render(
+    <React.StrictMode>
+        <Main />
     </React.StrictMode>
 )
