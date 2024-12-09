5a7537d55deb7412ba32a530ab5196489a63178c
Vinicius Egidio <me@vinicius.io>
2024-12-09 23:20:09 +0100

    build: Automate builds with Taskfile

--- cli/umd.go
+++ cli/umd.go
@@ -1,70 +1,70 @@
 package main
 
 import (
-    "cli/internal/spinner"
-    "context"
-    "github.com/pterm/pterm"
-    "github.com/vegidio/shared"
-    "github.com/vegidio/umd-lib"
-    "github.com/vegidio/umd-lib/event"
-    "math"
-    "strings"
-    "time"
+	"cli/internal/spinner"
+	"context"
+	"github.com/pterm/pterm"
+	"github.com/vegidio/shared"
+	"github.com/vegidio/umd-lib"
+	"github.com/vegidio/umd-lib/event"
+	"math"
+	"strings"
+	"time"
 )
 
 func startQuery(
-    url string,
-    directory string,
-    parallel int,
-    limit int,
-    extensions []string,
+	url string,
+	directory string,
+	parallel int,
+	limit int,
+	extensions []string,
 ) error {
-    msg := ""
-    queryCount := 0
-    var stopSpinner context.CancelFunc
+	msg := ""
+	queryCount := 0
+	var stopSpinner context.CancelFunc
 
-    u, err := umd.New(url, nil, func(ev event.Event) {
-        switch e := ev.(type) {
-        case event.OnExtractorFound:
-            pterm.Print("\n🌎 Website found: ", pterm.FgLightGreen.Sprintf(e.Name))
+	u, err := umd.New(url, nil, func(ev event.Event) {
+		switch e := ev.(type) {
+		case event.OnExtractorFound:
+			pterm.Print("\n🌎 Website found: ", pterm.FgLightGreen.Sprintf(e.Name))
 
-        case event.OnExtractorTypeFound:
-            pterm.Println("; extractor type:", pterm.FgLightYellow.Sprintf("%s", e.Type))
-            msg = pterm.Sprintf("📝 Querying %s %s for media files...", strings.ToLower(e.Type),
-                pterm.Bold.Sprintf(e.Name))
-            stopSpinner = spinner.CreateSpinner(msg, queryCount)
+		case event.OnExtractorTypeFound:
+			pterm.Println("; extractor type:", pterm.FgLightYellow.Sprintf("%s", e.Type))
+			msg = pterm.Sprintf("📝 Querying %s %s for media files...", strings.ToLower(e.Type),
+				pterm.Bold.Sprintf(e.Name))
+			stopSpinner = spinner.CreateSpinner(msg, queryCount)
 
-        case event.OnMediaQueried:
-            queryCount += e.Amount
-            spinner.UpdateSpinner(msg, int(math.Min(float64(queryCount), float64(limit))))
+		case event.OnMediaQueried:
+			queryCount += e.Amount
+			spinner.UpdateSpinner(msg, int(math.Min(float64(queryCount), float64(limit))))
 
-        case event.OnQueryCompleted:
-            stopSpinner()
-            time.Sleep(250 * time.Millisecond) // Wait for the spinner to finish
-        }
-    })
+		case event.OnQueryCompleted:
+			stopSpinner()
+			time.Sleep(250 * time.Millisecond) // Wait for the spinner to finish
+		}
+	})
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    resp, err := u.QueryMedia(limit, extensions, true)
-    if err != nil {
-        return err
-    }
+	resp, err := u.QueryMedia(limit, extensions, true)
+	if err != nil {
+		return err
+	}
 
-    startDownload(resp.Media, directory, parallel)
+	startDownload(resp.Media, directory, parallel)
 
-    pterm.Println("\n🌟 Done!")
-    return nil
+	pterm.Println("\n🌟 Done!")
+	return nil
 }
 
 func startDownload(media []umd.Media, directory string, parallel int) []shared.Download {
-    pb, _ := pterm.DefaultProgressbar.
-        WithTotal(len(media)).
-        Start("Downloading")
+	pb, _ := pterm.DefaultProgressbar.
+		WithTotal(len(media)).
+		Start("Downloading")
 
-    return shared.DownloadAll(media, directory, parallel, func(download shared.Download) {
-        pb.Increment()
-    })
+	return shared.DownloadAll(media, directory, parallel, func(download shared.Download) {
+		pb.Increment()
+	})
 }
