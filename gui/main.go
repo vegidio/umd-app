0f77e769b3a3c16e18ffdd9bfe8ed5891fb69a7e
Vinicius Egidio <me@vinicius.io>
2024-12-15 07:16:56 +0100

    fix(macOS): Remove window flickering when opening the app

--- gui/main.go
+++ gui/main.go
@@ -1,41 +1,45 @@
 package main
 
 import (
-    "embed"
-    log "github.com/sirupsen/logrus"
-    "io"
+	"embed"
+	log "github.com/sirupsen/logrus"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
+	"io"
 
-    "github.com/wailsapp/wails/v2"
-    "github.com/wailsapp/wails/v2/pkg/options"
-    "github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
 
 func main() {
-    // Disable logging
-    log.SetOutput(io.Discard)
+	// Disable logging
+	log.SetOutput(io.Discard)
 
-    // Create an instance of the app structure
-    app := NewApp()
+	// Create an instance of the app structure
+	app := NewApp()
 
-    // Create application with options
-    err := wails.Run(&options.App{
-        Title:  "Universal Media Downloader",
-        Width:  960,
-        Height: 720,
-        AssetServer: &assetserver.Options{
-            Assets: assets,
-        },
-        BackgroundColour: &options.RGBA{R: 1, G: 1, B: 1, A: 1},
-        OnStartup:        app.startup,
-        Bind: []interface{}{
-            app,
-        },
-    })
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "Universal Media Downloader",
+		Width:  960,
+		Height: 720,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		Mac: &mac.Options{
+			WebviewIsTransparent: true,
+		},
+		BackgroundColour: &options.RGBA{R: 1, G: 1, B: 1, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
 
-    if err != nil {
-        log.Error("Error:", err)
-    }
+	if err != nil {
+		log.Error("Error:", err)
+	}
 }
